7e8d11ea86785a0f132294644325981ac041b937
Damian Metheny <metheny555@outlook.com>
2025-04-16 23:13:25 -0400

    Damian's debugg lab 1

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,16 +1,18 @@
 function calculateDiscountedPrice(quantity, pricePerItem) {
-    let totalPrice = 0;
     debugger
-    for (let i = 1; i < quantity; i++) {
+    let totalPrice = 0;
+    
+    for (let i = 0; i < quantity; i++) {
         totalPrice += pricePerItem;
-    }
-
+    }  
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
 
     return totalPrice;
 }
+
+console.log(calculateDiscountedPrice(10, 20));
 
 
 
